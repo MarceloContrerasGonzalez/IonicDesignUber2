fdf00b41f203618476986868960fe8e19377441f
Jordanium01 <jo.urzua@duocuc.cl>
2022-09-01 00:00:59 -0400

    agregue cositas para el recovery

--- src/app/pages/recovpass/recovpass.page.ts
+++ src/app/pages/recovpass/recovpass.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recovpass',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recovpass.page.scss'],
 })
 export class RecovpassPage implements OnInit {
+  nombre_user:string="";
 
-  constructor() { }
+  constructor(
+    public toastController: ToastController,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  async presentToast(msg:string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  Recovery(){
+    if (this.validateUser(this.nombre_user)){
+      this.presentToast("Se a enviado un correo al correo asociado de :" + this.nombre_user)
+
+      //cambiar de pagina, pasar el parametro de state
+      this.router.navigate(['/login'])
+    }
+    
+  }
+
+  validateUser(value){
+    //Aqui se validaria que el usuario exista en la base de datos... a futuro
+    if(value == "" ){
+      return false;
+    }
+      
+      return true;
+    }
 }
