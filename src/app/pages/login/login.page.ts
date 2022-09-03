c6da6b2be52df64c5d534a79257ce7b149edc3bf
MarceloContrerasGonzalez <marc.contreras@duocuc.cl>
2022-09-02 20:51:39 -0400

    avance login +validacion

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,48 +1,63 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
-//import { ToastController } from '@ionic/angular';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-/*
-export class LoginPage implements OnInit {
-dato:any;
 
-constructor(private activeroute: ActivatedRoute, private router: Router) {
-	this.activeroute.queryParams.subscribe(params => {
-		if (this.router.getCurrentNavigation().extras.state) {
-			this.dato = this.router.getCurrentNavigation().extras.state.dato;
-			console.log(this.dato)
-		}
-	});
-}
-
-  ngOnInit() {
-  }
-
-}
-*/
 
 export class LoginPage implements OnInit {
-	dato:string;
-	
+	user ={
+		usuario: "",
+		password: ""
+	}
+
+
+	field: String = "";
 	constructor(
-		private router: Router
-	  ) {}
+		private router: Router,
+		public toastController: ToastController
+		) {}
 
 	  InSesion(){
-		//state para pasar parametros a la otra pagina
-		let navigationExtras: NavigationExtras={
-		  state: {dato: this.dato}
+		/* validacion */
+		if (this.validador(this.user)){
+			let navigationExtras: NavigationExtras={
+				state:{
+					user: this.user
+				}
+			};
+			this.router.navigate(['/home'], navigationExtras)
+		}else{
+			this.presentToast("Error En el campo: "+this.field);
 		}
-		//cambiar de pagina, pasar el parametro de state
-		this.router.navigate(['/home'], navigationExtras)
-	  }
+
+	  };
 	
+	  
+	 validador(model: any){
+		for (var [key, value] of Object.entries(model)) {
+			if (value == ""){
+				this.field = key;
+				return false;
+			}
+		}
+		return true;
+	 };
+	 
+	 
 	  ngOnInit() {
-	  }
+	  };
+
+	  async presentToast(msg:string) {
+		const toast = await this.toastController.create({
+		  message: msg,
+		  duration: 3500
+		});
+		toast.present();
+	  };
 	
 }
