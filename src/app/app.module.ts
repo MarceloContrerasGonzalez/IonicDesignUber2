33845592804b2929f94b8018ff48ba9abba98df7
Jordanium01 <jo.urzua@duocuc.cl>
2022-10-08 14:51:52 -0300

    decoracion buscar / viaje (no correo)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 //Importaciones angular material
 import { MatInputModule } from '@angular/material/input';
 /* import { MatDialogModule } from '@angular/material/dialog';
@@ -26,7 +25,9 @@ import { DialogComponent } from './components/shared/dialog/dialog.component'; *
     //MatDialogModule
   ],
   //entryComponents: [DialogComponent],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
