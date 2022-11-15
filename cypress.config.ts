177d07b575a2ca742b811564131ab332bdba0463
Marcelo Contreras <101838713+MarceloContrerasGonzalez@users.noreply.github.com>
2022-11-15 16:31:46 -0300

    cypress agregado e2e
    se agrego cypress para hacer las pruebas e2e

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,5 +3,14 @@ export default {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  },
+
+  "component": {
+    "devServer": {
+      "framework": "angular",
+      "bundler": "webpack"
+    },
+    "specPattern": "**/*.cy.ts"
   }
+  
 };
