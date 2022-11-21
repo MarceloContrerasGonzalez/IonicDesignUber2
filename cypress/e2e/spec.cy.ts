39c1a0fda92a813ec2bdab0a419d5bae9f055913
Marcelo Contreras <101838713+MarceloContrerasGonzalez@users.noreply.github.com>
2022-11-21 19:48:09 -0300

    Ultima Antes de la Firma

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -3,33 +3,44 @@ describe('e2e-prueba-movil', () => {
     cy.viewport('iphone-6')
   })
   it('Recuperar Contraseña', () => {
+
     //iniciar pagina
     cy.visit('http://localhost:8100/')
+
     //ir a recuperar contraseña
     cy.contains('¿Olvidaste tu contraseña?').click()
+    
     //revisar que sea la url correcta
     cy.url().should('include', '/recovpass')
+    
     //verificar mensaje de error en datos incorrectos (recuperar contraseña)
     cy.get('.recuperar_usuario').type('marc.contrera').should('have.value', 'marc.contrera')
+    cy.wait(700)
     cy.contains('Siguiente').click()
     cy.contains('Ok').click()
+    
     //verificar que la recuperacion de contraseña funcione
     cy.get('.recuperar_usuario').type('s').should('have.value', 'marc.contreras')
+    cy.wait(700)
     cy.contains('Siguiente').click()
     cy.contains('Ok').click()
+    
     //verificar que el inicio de sesion envie un mensaje de error
     cy.get('.ingresar_usuario').type('marc.contrera').should('have.value', 'marc.contrera')
     cy.get('.ingresar_contrasena').type('123456').should('have.value', '123456')
     cy.contains('Ingresar').click()
     cy.contains('Ok').click()
+    
     //verificar que se inicie sesion correctamente
     cy.get('.ingresar_usuario').type('s').should('have.value', 'marc.contreras')
     cy.get('.ingresar_contrasena').type('123456').should('have.value', '123456')
     cy.contains('Ingresar').click()
+    
     //verificar que se pueda navegar entre los tabs correctamente
     cy.contains('Conducir').click()
     cy.contains('Buscar viaje').click()
     cy.contains('Inicio').click()
+    
     //verificar que se pueda cerrar sesion correctamente
     cy.contains('cerrar sesion').click()
   })
