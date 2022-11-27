128c77495d25fb93379d31d1a3c2db277a56c81c
Marcelo Contreras <101838713+MarceloContrerasGonzalez@users.noreply.github.com>
2022-11-26 22:08:14 -0300

    version antes del areglo del joldan
    ola

--- cypress/e2e/spec_completo.cy.ts
+++ cypress/e2e/spec_completo.cy.ts
@@ -38,30 +38,39 @@ describe('e2e-prueba-movil', () => {
     
     //verificar que se pueda navegar entre los tabs correctamente
     cy.contains('Conducir').click()
+    //rellenar el formulario para crear un viaje
     cy.get('.pasajerostest').type('4')
     cy.get('.tarifatest').type('3300')
+    //abrir el maps e ingresar el destino
     cy.contains("Destino:").click()
-    cy.wait(1000)
+    cy.wait(1500)
     cy.get('.maptest').click('center')
-    cy.wait(1000)
+    cy.wait(1500)
     cy.contains("Aceptar").click()
+    //rellenar patente y datos extra del viaje
     cy.get('.patentetest').type('XX-XX-21')
     cy.get('.mat_area').type('viajeTesting')
+    //iniciar el viaje
     cy.contains("Preparar el viaje").click()
+    //navegar al tab de pedir viaje
     cy.contains('Buscar viaje').click()
     cy.get('.card_padding').click()
     cy.contains("Reservar").click()
     cy.wait(500)
+    //navegar al tab de conducir e iniciar el viaje
     cy.contains('Conducir').click()
     cy.wait(500)
     cy.contains('Empezar viaje').click()
     cy.wait(700)
+    //terminar el viaje
     cy.contains('Terminar el viaje').click()
     cy.wait(700)
+    //vamos a ver si el pasajero termino el viaje
     cy.contains('Buscar viaje').click()
     cy.wait(700)
     cy.get('.dialog_button').click()
     cy.wait(500)
+    //volvemos a inicio 
     cy.contains('Inicio').click()    
     //verificar que se pueda cerrar sesion correctamente
     cy.contains('cerrar sesion').click()
